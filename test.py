f22958f5f4c627f1e01a98cb5913bad4f656cf8b
Jan Knobloch <64230415+janknob@users.noreply.github.com>
2021-04-13 11:01:21 +0200

    Data Graph

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import plotly.express as px
 
+df = pd.read_csv ('https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/cases-rki-by-state.csv')
+print(df.to_string())
 
-
+fig = px.line(df, x = 'time_iso8601', y = 'DE-BY', title='Fallzahlen Bayern')
+fig.show()
