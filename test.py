251261745c3a2435bfeda0e7698cad0ed4bd0478
Jan Knobloch <64230415+janknob@users.noreply.github.com>
2021-04-26 11:17:55 +0200

    Graphs

--- test.py
+++ test.py
@@ -6,24 +6,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv ('kraj-okres-nakazeni-vyleceni-umrti (1).csv')
-df.drop(df.index[(df["okres_lau_kod"] != "CZ0411")],axis=0,inplace=True)
-print(df.to_string())
+df.columns = ['date', 'state', 'district', 'infected', 'healed', 'deaths']
+df.drop(df.index[(df["district"] != "CZ0411")],axis=0,inplace=True)
+df['date'] = pd.to_datetime(df['date'], format='%Y-%m-%d')
+#df.drop(df[(df['date'].df.year == 2021 | df['date'].df.month > 2 )])
+
+filtered_df = df.loc[(df['date'] >= '2020-03-01')
+                     & (df['date'] < '2021-03-01')]
+print(filtered_df.to_string())
 df_cleaned = df.dropna()
 
 """fig = px.line(df, x = 'datum', y = 'kumulativni_pocet_vylecenych', title='Fallzahlen Tirschenreuth - Cheb')
 fig.show()"""
 
 df2 = pd.read_csv('https://raw.githubusercontent.com/jgehrcke/covid-19-germany-gae/master/cases-rki-by-ags.csv')
+df2['time_iso8601'] = pd.to_datetime(df2['time_iso8601'])
+#df2['time_iso8601'] = pd.to_datetime(df.time_iso8601)
+df2['date_only'] = df2['time_iso8601'].dt.date
+df2['date_only'] = pd.to_datetime(df2['date_only'], format='%Y-%m-%d')
+filtered_df2 = df2.loc[(df2['date_only'] >= '2020-03-01')
+                     & (df2['date_only'] < '2021-03-01')]
 print(df2.to_string())
 
-fig = make_subplots(rows=1, cols = 2)
+#fig = make_subplots(rows=1, cols = 2)
+#fig = go.Figure()
+#fig.add_trace(go.Scatter(x=filtered_df['date'],
+#                         y=filtered_df['infected'].diff()),
+#                         row=1, col=1)
+#fig.add_trace(go.Scatter(x=filtered_df2['date_only'],
+#                         y=filtered_df2['9377'].diff()),
+#                         row=1, col=2
+#                        )
+#fig.update_layout(height=600, width=800, title_text="Cheb im Vergleich zu Tirscheinreuth")
+#fig.show()
 
-fig.add_trace(go.Scatter(x=df['datum'],
-                         y=df['kumulativni_pocet_vylecenych'].diff()),
-                         row=1, col=1)
-fig.add_trace(go.Scatter(x=df2['time_iso8601'],
-                         y=df2['9377'].diff()),
-                         row=1, col=2
-                         )
-fig.update_layout(height=600, width=800, title_text="Cheb im Vergleich zu Tirscheinreuth")
+fig = go.Figure()
+fig.add_trace(
+    go.Scatter(
+        x = filtered_df['date'],
+        y = filtered_df['infected'].diff(),
+        name="Cheb"
+    )
+)
+fig.add_trace(
+    go.Scatter(
+        x = filtered_df2['date_only'],
+        y = filtered_df2['9377'].diff(),
+        name="Tirschenreuth"
+    )
+)
+fig.update_yaxes( dtick=10)
+
+fig.update_xaxes(
+    dtick="M1",
+    tickformat="%b\n%Y")
+fig.update_layout(height=800, width=1500, title_text="Cheb im Vergleich zu Tirscheinreuth")
 fig.show()
