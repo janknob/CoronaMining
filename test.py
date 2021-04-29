71e78683dbc0f922348bbcd8a03df6058e0d0412
Jan Knobloch <64230415+janknob@users.noreply.github.com>
2021-04-29 12:21:06 +0200

    Variables

--- test.py
+++ test.py
@@ -22,18 +22,46 @@ df2['date_de'] = pd.to_datetime(df2['date_de'], format='%Y-%m-%d')
 filtered_df2 = df2.loc[(df2['date_de'] >= '2020-03-02')
                      & (df2['date_de'] < '2021-03-02')]
 
+#DE districts
+Freyung_Grafenau = "9272"
+Regen = "9276"
+Cham = "9372"
+Schwandorf = "9376"
+Neudstadt_Waldnaab = "9374"
+Tirschenreuth = "9377"
+Wunsiedel = "9479"
+Hof = "9475"
+Vogtlandkreis = "14523"
+Erzgebirgskreis = "14521"
+Mittelsachsen = "14522"
+Sächsische_Schweiz = "14628"
+Bautzen = "14625"
+Görlitz = "14626"
 
+#CZ districts
 
-
+Prachatitz = "CZ0315" #Prachatitz (Prachatice)
+Klattau = "CZ0315" #Klattau (Klatovy)
+Taus = "CZ0321" #Taus (Domažlice)
+Tachau = "CZ0327" #Tachau (Tachov)
+Eger = "CZ0411" #Eger (Cheb)
+Falkenau = "CZ0413" #Falkenau (Sokolov)
+Karlsbad = "CZ0412" #Karlsbad (Karlovy Vary)
+Komotau = "CZ0422" #Komotau (Chomutov)
+Brux ="CZ0425" #Brux (Most)
+Teplitz_Schönau = "CZ0426" #Teplitz-Schönau (Teplice)
+Aussig = "CZ0427" #Aussig (Ústí nad Labem)
+Tetschen = "CZ0421" #Tetschen (Děčín)
+Böhmisch_Leipa = "CZ0511" #Böhmisch Leipa (Česká Lípa)
 
 def vergleicheTirschenreuthMitCheb():
-    filtered_df.drop(filtered_df.index[(filtered_df["district"] != "CZ0411")], axis=0, inplace=True)  # Funktion für Landrkeis Auswahl
+    filtered_df.drop(filtered_df.index[(filtered_df["district"] != Eger)], axis=0, inplace=True)  # Hier Landkreis Schlüssel ändern (Hier Cheb)
 
     filtered_df.drop(filtered_df.columns.difference(['date_cz', 'district_cz_infected_number']), 1, inplace=True)
     print(filtered_df.to_string())
     df_cleaned = df.dropna()
 
-    filtered_df2['district_de_infected_number'] = filtered_df2['9377']  # Hier Landkreis Schlüssel ändern (Hier Tirschenreuth)
+    filtered_df2['district_de_infected_number'] = filtered_df2[Tirschenreuth]  # Hier Landkreis Schlüssel ändern (Hier Tirschenreuth)
     filtered_df2.drop(filtered_df2.columns.difference(['date_de', 'district_de_infected_number']), 1, inplace=True)
 
     print(filtered_df.to_string())
